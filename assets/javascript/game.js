3cb3bc996c05db4f027e9f762b609e694c65b63e
Zac Seewald <zs@Zacs-MacBook-Pro.local>
2018-11-04 22:27:02 -0600

    stored random numbers in an object to call on later

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,10 +3,23 @@ $(document).ready(function () {
     //      wins and losses are reset to 0
         var  wins = 0;
         var losse = 0;
+        //      computor choses a random number
         var compRandomNum = Math.floor(Math.random() * 100);
 
-        console.log(compRandomNum);
-    //      computor choses a random number
+        var crystalNums = 
+            {
+                red: Math.floor(Math.random() * 100),
+                blue: Math.floor(Math.random() * 100),
+                purple: Math.floor(Math.random() * 100),
+                green:  Math.floor(Math.random() * 100)
+            };
+        
+        // var red = Math.floor(Math.random() * 100);
+        // var blue = Math.floor(Math.random() * 100);
+        // var purple = Math.floor(Math.random() * 100);
+        // var green = Math.floor(Math.random() * 100);
+        console.log(crystalNums.red,crystalNums.blue, crystalNums.purple,crystalNums.green);
+    
     //      computor assigns a nuber to the user and all the crystals
     //      user clicks on a crystal and the value of said crystal is displayed
     //      user click another crystal and that value is added to the first val, and added to the displayed score
