e00020b8d12de3a75d249b68ac1fd4c170cbc100
Zac Seewald <zs@Zacs-MacBook-Pro.local>
2018-11-04 22:06:49 -0600

    added js psudo code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,14 @@
 $(document).ready(function () {
-console.log("ther");
+    //  steps:
+    //      wins and losses are reset to 0
+    //      computor choses a random number
+    //      computor assigns a nuber to the user and all the crystals
+    //      user clicks on a crystal and the value of said crystal is displayed
+    //      user click another crystal and that value is added to the first val, and added to the displayed score
+    //      if the total score is less that the random value, the user chooese another crystal and that is added to the displayed score and so on until the total score either === the computor's random number or is greater than that num
+    //      if the user total score === to the random num => add to the wins and reset the game.
+    //      if the user total score is > the the random num => add to the losses and reset the game.
+    // there should be a button at the bottum to reset the game w/out refresshing the page.
+
+
 });
